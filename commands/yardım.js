4f873bd7786a452a3f5bec23f52b94994026e6ec
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-08-11 17:18:28 +0300

    Update yardım.js

--- commands/yardım.js
+++ commands/yardım.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
 
     const embed = new EmbedBuilder()
     .setTitle("Godzilla - Yardım!")
-    .setDescription(`${prefix}ticket-yetkilisi\n${prefix}ticket-oluştur Buton Üzerinde Yazıcak Yazı + Embed Mesaj Yazısı`)
+    .setDescription(`${prefix}ticket-yetkilisi\n${prefix}ticket-oluştur Buton Üzerinde Yazıcak Yazı + Embed Mesaj Yazısı\n${prefix}ticket-log`)
     .setColor("#007fff")
     .setTimestamp()
     return message.channel.send({embeds : [embed]});
