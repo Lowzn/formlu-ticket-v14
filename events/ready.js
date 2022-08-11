297b7d2e7b3e90758e864aba61eac9a8a3f0872b
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-08-11 15:18:03 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const client = require("../index");
+const { Collection } = require("discord.js")
+const fs = require("fs")
+const config = require("../config.js")
+let prefix = config.prefix
+client.on("ready", () => {
+client.user.setActivity(`${prefix}yardım`)
+
+client.commands = new Collection();
+client.aliases = new Collection();
+fs.readdir("./commands/", (err, files) => {
+if (err) console.error(err);
+console.log(`Toplam ${files.length} Komutun Var!`);
+files.forEach(f => {
+let props = require(`../commands/${f}`);
+    
+console.log(`${props.help.name} Adlı Komut Yüklendi!`);
+    
+client.commands.set(props.help.name, props);
+props.conf.aliases.forEach(alias => {
+client.aliases.set(alias, props.help.name);
+});
+});
+});
+
+});
